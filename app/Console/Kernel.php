40486c1d615ca3a1e471312e0c5c4043a0ce70e6
Corazon Espinado <rvr@arguss.lv>
2025-10-16 23:48:37 +0300

    chadule cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -23,9 +23,9 @@ class Kernel extends ConsoleKernel
         $schedule->command('expiring-docs:notify')->twiceDaily(8, 15);
         $schedule->command('expiring-docs:notify')->dailyAt('12:30');
         $schedule->command('expiring-docs:notify')->dailyAt('14:00');
-        $schedule->command('expiring-docs:notify')->dailyAt('23:42');
-        $schedule->command('expiring-docs:notify')->dailyAt('23:45');
-        $schedule->command('expiring-docs:notify')->dailyAt('23:46');
+        $schedule->command('expiring-docs:notify')->dailyAt('23:50');
+        $schedule->command('expiring-docs:notify')->dailyAt('23:52');
+        $schedule->command('expiring-docs:notify')->dailyAt('23:54');
 
         \Log::info('✅ Scheduler is running fine: ' . now());
     }
