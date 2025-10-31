1173a6c08f4e07db80c0aac3b60594b72a6ae7f2
Corazon <rvr@arguss.lv>
2025-10-31 11:08:03 +0200

    pwa

---
+++ config/laravelpwa.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'name' => 'Fleet Manager',
+    'short_name' => 'Fleet',
+    'start_url' => '/',
+    'background_color' => '#0d6efd',
+    'theme_color' => '#0d6efd',
+    'display' => 'standalone',
+    'orientation'=> 'portrait',
+    'status_bar'=> 'black',
+    'icons' => [
+        '72x72' => ['path' => '/images/icons/fleet.png', 'purpose' => 'any'],
+        '96x96' => ['path' => '/images/icons/fleet.png', 'purpose' => 'any'],
+        '128x128' => ['path' => '/images/icons/fleet.png', 'purpose' => 'any'],
+        '144x144' => ['path' => '/images/icons/fleet.png', 'purpose' => 'any'],
+        '152x152' => ['path' => '/images/icons/fleet.png', 'purpose' => 'any'],
+        '192x192' => ['path' => '/images/icons/fleet.png', 'purpose' => 'any'],
+        '384x384' => ['path' => '/images/icons/fleet.png', 'purpose' => 'any'],
+        '512x512' => ['path' => '/images/icons/fleet.png', 'purpose' => 'any'],
+    ],
+];
