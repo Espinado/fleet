98263c8a45317268edfbe35d7fae450372c865bb
Corazon <rvr@arguss.lv>
2025-11-18 20:54:46 +0200

    cron test

--- routes/console.php
+++ routes/console.php
@@ -8,9 +8,13 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
+protected function schedule(Schedule $schedule)
+{
    $schedule->command('expiring-docs:notify')->dailyAt('09:00');
     $schedule->command('expiring-docs:notify')->dailyAt('11:00');
     $schedule->command('expiring-docs:notify')->dailyAt('12:00');
        $schedule->command('expiring-docs:notify')->dailyAt('09:00');
     $schedule->command('expiring-docs:notify')->dailyAt('21:15');
     $schedule->command('expiring-docs:notify')->dailyAt('21:30');
+
+    }
