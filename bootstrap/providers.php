c4b42c4cf3a2457a8cbb9230fbc5d1635ba2a52d
Corazon <rvr@arguss.lv>
2025-11-20 00:55:29 +0200

    push

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
       LaravelPWA\Providers\LaravelPWAServiceProvider::class,
+       NotificationChannels\WebPush\WebPushServiceProvider::class,
 ];
