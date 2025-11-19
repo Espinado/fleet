7c48d8867d4cc85832fbee4a6f656c570b86b065
Corazon <rvr@arguss.lv>
2025-11-20 00:38:20 +0200

    push

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
+      ->withProviders([
+        NotificationChannels\WebPush\WebPushServiceProvider::class,
+    ])
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
