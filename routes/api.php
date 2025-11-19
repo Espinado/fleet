b8dfa6fb94a0ffc7624d9f8c9fc8c5cc879abdf8
Corazon <rvr@arguss.lv>
2025-11-19 23:24:56 +0200

    push

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth')->post('/push/subscribe', function (Request $request) {
+Route::middleware('auth:web')->post('/push/subscribe', function (Request $request) {
     $user = $request->user();
 
     $data = $request->validate([
